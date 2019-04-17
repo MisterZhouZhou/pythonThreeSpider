99a9efa4f49ae16529937df0738879d424591271
zhouwei <m15038960016@163.com>
2019-04-17 19:06:29 +0800

    update

--- douyinspider/douyin.py
+++ douyinspider/douyin.py
@@ -14,23 +14,19 @@ from douyinspider.config import common_headers
 # 递归获取所有的提交视频
 def get_aweme_list():
     user_video_list = []
-    url='https://api.amemv.com/aweme/v1/music/aweme/'
-    query = {
-        'device_id': '33333333',
-        'music_id': 'bed600035a486a062310',
-        'count': '18',
-    }
+    url='https://aweme.snssdk.com/aweme/v1/challenge/aweme/?iid=30373511894&device_id=35781128184&os_api=18&app_name=aweme&channel=App%20Store&idfa=811A8841-030F-4AEA-B934-C2A56489C32D&device_platform=iphone&build_number=17805&vid=A4BB3AF4-7981-4805-995B-78419881DC11&openudid=99bf2b608173e21cefd562496d2cf21fa8eba580&device_type=iPhone7,2&app_version=1.7.8&version_code=1.7.8&os_version=11.3&screen_width=750&aid=1128&ac=WIFI&ch_id=1574030716416014&count=21&cursor=0&pull_type=2&query_type=0&type=5&mas=001469ed4a7a3f61de046e21c8c7ae8bcb64a983471da18ec0c8d1&as=a1c5206d9e676a38ee4960&ts=1524500606'
     offset, count = 0, 0
     while True:
         # define cursor
-        query['cursor'] = str(offset)
-        result = fetch(URL.music2video_url(), params=query, headers=common_headers, verify=False)
+        result = fetch(url, headers=person_post_headers, verify=False)
         print(result)
         if result.get('has_more') != 1:
             break
 
 
 if __name__ == '__main__':
+    get_aweme_list()
+
 
     # for musicCollection in music_collection():
     #     if isinstance(musicCollection, MusicCollection):
@@ -43,7 +39,7 @@ if __name__ == '__main__':
     # get_aweme_list()
     # downloadCategory()
     # 拉取抖音用户列表
-    person()
+    # person()
     # 下载陈赫喜欢的视频
     # downloadFavorite(84990209480, '陈赫')
     # 下载陈赫上传的视频
